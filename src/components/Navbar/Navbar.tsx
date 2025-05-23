969c44a9bf0af19ef51ccbb22f8f0c6392daac3e
Saran Pariyar <spariyar05@gmail.com>
2025-05-23 22:56:36 +0545

    sub-menu in nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,53 +1,86 @@
 // import Image from "next/image";
 import { NavLinkInterface } from "@/types/navbarTypes";
+import Link from "next/link";
 
-const navLinks: NavLinkInterface[] = [
+const navLinksItems: NavLinkInterface[] = [
     {
         name: 'Home',
         path: '/',
         isActive: (pathname: string) => pathname === '/',
     },
     {
-        name: 'Test',
-        path: '/',
-        isActive: (pathname: string) => pathname === '/test',
+        name: 'Tests',
+        path: '/tests',
+        isActive: (pathname: string) => pathname === '/tests',
     },
     {
         name: 'Categories',
-        path: '/',
-        subMenu:[],
+        path: '/categories',
+        subMenu: [{
+            name: 'Category 1',
+            path: '/categories/category1',
+            isActive: (pathname: string) => pathname === '/categories/category1',
+        }],
         isActive: (pathname: string) => pathname === '/categories',
     },
     {
         name: 'About',
-        path: '/',
+        path: '/about',
         isActive: (pathname: string) => pathname === '/about',
     },
     {
         name: 'Sign In',
-        path: '/',
+        path: '/sign-in',
         isActive: (pathname: string) => pathname === '/sign-in',
     },
 ]
 
-export default function  Navbar() {
+export default function Navbar() {
 
     return (
         <nav>
-            <div className="nav-logo">
+            <div className="nav-logo-container">
                 {/* <Image
                     src="/logo.png"
                     alt="Logo"
                     width={100}
                     height={50}
                 /> */}
+                <h3>Logo</h3>
             </div>
 
-            <div className="nav-links-section">
-                
-                    this is a nav.
+            <div className="nav-list-container">
 
-                    {navLinks[1].name}
+                this is a nav.
+
+                <ul className="nav-list">
+
+                    {
+                        navLinksItems.map((item, index) => {
+                            return (
+                                <li key={item.path}>
+                                    <Link href={item.path} key={index} > {item.name} </Link>
+                                    {
+                                        item.subMenu && item.subMenu.length > 0 && (
+                                            <ul className="sub-menu">
+                                                {
+                                                    item.subMenu.map((item) => {
+                                                        return <Link href={item.path} key={item.path}>{item.name}</Link>
+                                                    })
+                                                }
+
+                                            </ul>
+                                        )
+                                    }
+                                </li>
+                            )
+                        })
+
+
+
+                    }
+
+                </ul>
             </div>
         </nav>
     )
--- src/types/navbarTypes.ts
+++ src/types/navbarTypes.ts
@@ -1,6 +1,14 @@
+import path from "path";
+
 export interface NavLinkInterface {
     name: string;
     path: string;
-    subMenu?: []; // Optional sub-menu items
+    subMenu?: [
+        {
+            name: string;
+            path: string;
+            isActive: (pathname: string) => boolean
+        }
+    ]; // Optional sub-menu items
     isActive?: (pathname: string) => boolean; // Optional function to determine active state
 }
