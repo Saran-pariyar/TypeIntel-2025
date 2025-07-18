428af9e67f25b6278b10156342a51983d5c18e51
Saran Pariyar <spariyar05@gmail.com>
2025-07-18 21:05:09 +0545

    Vercel Deployment fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,44 +1,26 @@
 "use client";
 
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import Link from 'next/link'
 
 const Navbar = () => {
     const [isNavOpen, setIsNavOpen] = useState(false);
 
-    // useEffect(() => {
-    //     const hideNavOnLinkClick = () => {
-    //         if (isNavOpen) {
-    //             setIsNavOpen(false);
-    //         }
-    //     }
 
-    //     document.querySelectorAll('.nav-links').forEach(link => {
-    //         link.addEventListener('click', hideNavOnLinkClick);
-    //     }
-    //     )
-
-    // }, []);
 const navItems = [
     {href : "/Categories", label: "Categories" },
     { href: "/about", label: "About" },
     { href: "/contact", label: "Contact" },
 ]
 
-    const handleNavToggle = () => {
-        setIsNavOpen(!isNavOpen);
-    }
-
-
-
     return (
         <nav className="navbar">
             <div className='nav-logo-container'>
 
-                <a href="/" className='nav-logo' aria-label="TypeIntel Home">
+                <Link href="/" className='nav-logo' aria-label="TypeIntel Home">
                     TypeIntel
-                </a>
+                </Link>
             </div>
             <div className={`hamburger-menu${isNavOpen ? " cross" : ""}`} id="hamburger-menu" onClick={() => setIsNavOpen(!isNavOpen)}>
                 <span className="ham-line ham-line1"></span>
