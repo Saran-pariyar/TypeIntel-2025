7b7150294f8cfdcbaefea4482c7ed92809e7ff9c
Saran Pariyar <spariyar05@gmail.com>
2025-07-18 21:03:12 +0545

    responsive nav fix

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+"use client";
+
+import React, { useState, useEffect } from 'react'
 import './navbar.css'
 import Link from 'next/link'
 
 const Navbar = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    // useEffect(() => {
+    //     const hideNavOnLinkClick = () => {
+    //         if (isNavOpen) {
+    //             setIsNavOpen(false);
+    //         }
+    //     }
+
+    //     document.querySelectorAll('.nav-links').forEach(link => {
+    //         link.addEventListener('click', hideNavOnLinkClick);
+    //     }
+    //     )
+
+    // }, []);
+const navItems = [
+    {href : "/Categories", label: "Categories" },
+    { href: "/about", label: "About" },
+    { href: "/contact", label: "Contact" },
+]
+
+    const handleNavToggle = () => {
+        setIsNavOpen(!isNavOpen);
+    }
+
+
+
     return (
         <nav className="navbar">
             <div className='nav-logo-container'>
@@ -11,28 +40,27 @@ const Navbar = () => {
                     TypeIntel
                 </a>
             </div>
-            <div className="hamburger-menu" id="hamburger-menu">
-                <span className="ham-line"></span>
-                <span className="ham-line"></span>
-                <span className="ham-line"></span>
+            <div className={`hamburger-menu${isNavOpen ? " cross" : ""}`} id="hamburger-menu" onClick={() => setIsNavOpen(!isNavOpen)}>
+                <span className="ham-line ham-line1"></span>
+                <span className="ham-line ham-line2"></span>
+                <span className="ham-line ham-line3"></span>
 
             </div>
 
-            <ul className='nav-list'>
+            <ul className={`nav-list` + (isNavOpen ? ' nav-show' : '')}>
+
+                {navItems.map((item, index) => (
+                    <li key={index}>
+                        <Link className='nav-links' href={item.href} onClick={() => setIsNavOpen(false)}>
+                            {item.label}
+                        </Link>
+                    </li>
+                ))}
                 <li>
-                    <Link className='nav-links' href="/Categories">Categories</Link>
-                </li>
-                <li>
-                    <Link className='nav-links' href="/about">About</Link>
-                </li>
-                <li>
-                    <Link className='nav-links' href="/contact">Contact</Link>
-                </li>
-                <li>
-                    <button className='sign-in-btn nav-links' type="button"> Sign In </button>
+                    <button className='sign-in-btn nav-links' type="button" onClick={() => setIsNavOpen(false)}> Sign In </button>
                 </li>
             </ul>
-            
+
         </nav>
     )
 }
