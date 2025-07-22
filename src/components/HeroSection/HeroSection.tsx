83772d8afecc49057d212aebedd49e828d50bccc
Saran Pariyar <spariyar05@gmail.com>
2025-07-22 23:53:11 +0545

    Hero section progress

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -3,9 +3,20 @@ import './hero.css';
 
 const HeroSection = () => {
   return (
-  <div>
+  <section className='hero-section'>
+    <div className="hero-section-container">
+
+      <h1 className='hero-title'>Type Smarter.</h1>
+      <h2 className='hero-second-title'>Learn Faster.</h2>
+      <p className="hero-desc">Practice typing with frontend intervie questions and web development topics. Boost your typing speed while preparing for your next big opportunity.</p>
+
+      <div className="hero-btn-container">
+        <button className="start-typing-btn">Start Typing</button>
+        <button className="view-demo-btn">View Demo </button>
+      </div>
+    </div>
     
-  </div>
+  </section>
   );
 };
 
