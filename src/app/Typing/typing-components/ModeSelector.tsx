bacd821f07da7d4510e42e04bacf949c43d07b09
Saran Pariyar <spariyar05@gmail.com>
2025-12-09 22:22:30 +0545

    Timer & question buttons

--- src/app/Typing/typing-components/ModeSelector.tsx
+++ src/app/Typing/typing-components/ModeSelector.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
 const ModeSelector = () => {
+
+  // when we select timer module, a selection section will apper below it, when questoin mode toolbar, selection box will appear 
   return (
-    <div>ModeSelector</div>
+
+    <div className='typing-box-container'>
+      {/* Mode selector */}
+      <div className="mode-left-section">
+        <div className="timer-question-mode-container">
+          <div className="timer-mode-button mode-button">Timer Mode</div>
+          <div className="question-mode-button mode-button">Question Mode</div>
+
+        </div>
+<div className="just-answer-or-question-selector">
+{/* if we select timer, then the width will contain box of whether time, or question number if select question  */}
+</div>
+      </div>
+      <div className="mode-right-section"></div>
+    </div>
   )
 }
 
