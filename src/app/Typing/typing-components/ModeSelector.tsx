c9527c57bfa8ad71e4aaa70ddc5677a7f36b6f22
Saran Pariyar <spariyar05@gmail.com>
2025-12-10 21:50:17 +0545

    Progress on Typing mode switch and Initialized Shadcn for switch

--- src/app/Typing/typing-components/ModeSelector.tsx
+++ src/app/Typing/typing-components/ModeSelector.tsx
@@ -1,3 +1,4 @@
+import { Switch } from '@/components/ui/switch'
 import React from 'react'
 
 const ModeSelector = () => {
@@ -5,7 +6,7 @@ const ModeSelector = () => {
   // when we select timer module, a selection section will apper below it, when questoin mode toolbar, selection box will appear 
   return (
 
-    <div className='typing-box-container'>
+    <div className='typing-box-container mode-selector-container'>
       {/* Mode selector */}
       <div className="mode-left-section">
         <div className="timer-question-mode-container">
@@ -13,8 +14,10 @@ const ModeSelector = () => {
           <div className="question-mode-button mode-button">Question Mode</div>
 
         </div>
-<div className="just-answer-or-question-selector">
+<div className="just-answer-or-question-selector-container">
 {/* if we select timer, then the width will contain box of whether time, or question number if select question  */}
+<p className='label'>Type Questions + Answers  </p>
+<Switch id="typing-mode-switch" />
 </div>
       </div>
       <div className="mode-right-section"></div>
